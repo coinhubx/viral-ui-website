08ad14abdf41f00accc9571705df7fef7d5a29a6
Cole Blender <coleblender@gmail.com>
2024-04-18 08:48:32 -0700

    test registry

--- registry/ui.ts
+++ registry/ui.ts
@@ -2,12 +2,6 @@ import { Registry } from "@/registry/schema"
 
 export const ui: Registry = [
   {
-    name: "accordion",
-    type: "components:ui",
-    dependencies: ["@radix-ui/react-accordion"],
-    files: ["ui/accordion.tsx"],
-  },
-  {
     name: "alert",
     type: "components:ui",
     files: ["ui/alert.tsx"],
