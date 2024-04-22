175ec5aeda114afac87a66a8f9ed59d219c7dbe9
Cole Blender <coleblender@gmail.com>
2024-04-22 12:00:46 -0700

    update route handler

--- src/app/api/components/route.ts
+++ src/app/api/components/route.ts
@@ -1,23 +1,26 @@
 import db from "@/db";
 import { components } from "@/db/schemas/components";
 import { users } from "@/db/schemas/users";
-import { and, eq } from "drizzle-orm";
+import { and, eq, inArray } from "drizzle-orm";
 
 export async function POST(req: Request) {
-  const { username, fileName } = await req.json();
+  const { username, fileNames } = await req.json();
 
   const res = await db
     .select()
     .from(components)
     .leftJoin(users, eq(users.id, components.userId))
     .where(
-      and(eq(users.username, username), eq(components.fileName, fileName)),
+      and(
+        eq(users.username, username),
+        inArray(components.fileName, fileNames),
+      ),
     );
 
-  let component = null;
+  let output = null;
   if (res.length) {
-    component = res[0].components;
+    output = res.map((r) => r.components);
   }
 
-  return Response.json(component);
+  return Response.json(output);
 }
