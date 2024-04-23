28df56f451d5a8b1aa2e44da540241efec0130d4
Cole Blender <coleblender@gmail.com>
2024-04-22 17:21:47 -0700

    fix header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -77,7 +77,7 @@ async function Header() {
         </SheetContent>
       </Sheet>
 
-      <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:gap-2 lg:gap-4">
+      <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:w-fit md:gap-2 lg:gap-4">
         <ModeToggle />
 
         <UserButton user={user} />
