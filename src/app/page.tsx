1c4f11307ac7876e2f3236bc5d3f5cd465626a20
Cole Blender <coleblender@gmail.com>
2024-04-22 19:20:32 -0700

    fix sidebar on route change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,120 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
 function HomePage() {
   return (
-    <main className="mt-10 flex flex-col items-center px-4">HomePage</main>
+    <main className="mt-10 flex flex-col items-center px-4">
+      <Tabs defaultValue="hot" className="flex w-full flex-col items-center">
+        <div className="flex items-center">
+          <TabsList>
+            <TabsTrigger value="hot">Hot</TabsTrigger>
+            <TabsTrigger value="latest">Latest</TabsTrigger>
+            <TabsTrigger value="allTime">All Time</TabsTrigger>
+          </TabsList>
+        </div>
+        <TabsContent value="hot">
+          <Card x-chunk="">
+            <CardHeader className="px-7">
+              <CardTitle>Orders</CardTitle>
+              <CardDescription>Recent orders from your store.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Customer</TableHead>
+                    <TableHead className="hidden sm:table-cell">Type</TableHead>
+                    <TableHead className="hidden sm:table-cell">
+                      Status
+                    </TableHead>
+                    <TableHead className="hidden md:table-cell">Date</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow className="bg-accent">
+                    <TableCell>
+                      <div className="font-medium">Liam Johnson</div>
+                      <div className="hidden text-sm text-muted-foreground md:inline">
+                        liam@example.com
+                      </div>
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">Sale</TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      <Badge className="text-xs" variant="secondary">
+                        Fulfilled
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      2023-06-23
+                    </TableCell>
+                    <TableCell className="text-right">$250.00</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="latest">
+          <Card x-chunk="">
+            <CardHeader className="px-7">
+              <CardTitle>Orders</CardTitle>
+              <CardDescription>Recent orders from your store.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Customer</TableHead>
+                    <TableHead className="hidden sm:table-cell">Type</TableHead>
+                    <TableHead className="hidden sm:table-cell">
+                      Status
+                    </TableHead>
+                    <TableHead className="hidden md:table-cell">Date</TableHead>
+                    <TableHead className="text-right">Amount</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  <TableRow className="bg-accent">
+                    <TableCell>
+                      <div className="font-medium">Liam Johnson</div>
+                      <div className="hidden text-sm text-muted-foreground md:inline">
+                        liam@example.com
+                      </div>
+                    </TableCell>
+                    <TableCell className="hidden sm:table-cell">Sale</TableCell>
+                    <TableCell className="hidden sm:table-cell">
+                      <Badge className="text-xs" variant="secondary">
+                        Fulfilled
+                      </Badge>
+                    </TableCell>
+                    <TableCell className="hidden md:table-cell">
+                      2023-06-23
+                    </TableCell>
+                    <TableCell className="text-right">$250.00</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </main>
   );
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import { Menu } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import ModeToggle from "./ModeToggle";
 import UserButton from "./UserButton";
 import { getUser } from "@/lib/auth";
+import Sidebar from "./Sidebar";
 
 async function Header() {
   const user = await getUser();
@@ -39,43 +37,7 @@ async function Header() {
         </Link>
       </nav>
 
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Toggle navigation menu</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="/"
-              className="flex items-center gap-2 text-lg font-semibold"
-            >
-              ViralUI
-              <span className="sr-only">ViralUI</span>
-            </Link>
-            <Link
-              href="/add-component"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              Add Component
-            </Link>
-            <Link
-              href="/my-components"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              My Components
-            </Link>
-            <Link
-              href="/my-likes"
-              className="text-muted-foreground hover:text-foreground"
-            >
-              My Likes
-            </Link>
-          </nav>
-        </SheetContent>
-      </Sheet>
+      <Sidebar />
 
       <div className="flex w-full items-center justify-end gap-4 md:ml-auto md:w-fit md:gap-2 lg:gap-4">
         <ModeToggle />
